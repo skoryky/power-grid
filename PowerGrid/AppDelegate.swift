5ce4f236411a6be299a9a6cd0ef252a3844478fb
Linda Nguyen <skoryky@gmail.com>
2015-05-28 22:29:00 -0700

    Add skeleton project

---
+++ PowerGrid/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PowerGrid
+//
+//  Created by Linda Nguyen on 5/28/15.
+//  Copyright (c) 2015 Linda Nguyen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+
+}
---
+++ PowerGrid/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PowerGrid
+//
+//  Created by Linda Nguyen on 5/28/15.
+//  Copyright (c) 2015 Linda Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
