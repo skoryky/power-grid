8d5938e482c909a505849f0aa0d6e14bc95291d1
Linda Nguyen <skoryky@gmail.com>
2015-05-28 23:37:33 -0700

    Add Resource enum

---
+++ PowerGrid/Models/Resource.swift
@@ -0,0 +1,16 @@
+//
+//  Resource.swift
+//  PowerGrid
+//
+//  Created by Linda Nguyen on 5/28/15.
+//  Copyright (c) 2015 Linda Nguyen. All rights reserved.
+//
+
+import Foundation
+
+enum Resource {
+  case Coal
+  case Garbage
+  case Oil
+  case Uranium
+}
